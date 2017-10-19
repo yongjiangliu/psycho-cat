ce508b02ec7cf83ac75d445c1b2ee86c376a5a78
Beichen Li <bclicn@gmail.com>
2017-10-19 14:55:55 +0800

    delete old upload

--- public/js/test.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by root on 11/4/16.
- */
-function greeter(person) {
-    return "Hello, " + person;
-}
-var user = "Jane User";
-document.body.innerHTML = greeter(user);
-//# sourceMappingURL=test.js.map
